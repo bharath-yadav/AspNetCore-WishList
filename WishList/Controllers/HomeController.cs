3b1486f199e16eda93f02d8201543a71f8997024
Bharath Yadav <43810548+bharath-yadav@users.noreply.github.com>
2021-04-23 22:38:36 +0530

    Task 2a

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,12 +10,10 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            //return View();
             return Index();
         }
         public IActionResult Error()
         {
-            //return View();
             return Error();
         }
     }
